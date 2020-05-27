f626ca68e42969b694dc5c368af64ceaeb43c4e1
Malik <hiraev.github@gmail.com>
2020-05-27 21:36:43 +0300

    Find classes and typealiases in scopes

--- libsl-kt-plugin/src/main/kotlin/ru/hiraev/libls/kotlinc/plugin/LibSLProjectProcessor.kt
+++ libsl-kt-plugin/src/main/kotlin/ru/hiraev/libls/kotlinc/plugin/LibSLProjectProcessor.kt
@@ -1,52 +1,66 @@
 package ru.hiraev.libls.kotlinc.plugin
 
 import kastree.ast.Node
-import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptorWithVisibility
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
 import org.jetbrains.kotlin.resolve.DescriptorUtils
 import org.jetbrains.kotlin.resolve.descriptorUtil.isEffectivelyPublicApi
+import org.jetbrains.kotlin.resolve.lazy.descriptors.LazyClassDescriptor
+import org.jetbrains.kotlin.resolve.lazy.descriptors.LazyTypeAliasDescriptor
 import org.jetbrains.kotlin.resolve.scopes.MemberScope
 
 object LibSLProjectProcessor {
 
     private val structuredProject = mutableListOf<Node.File>()
 
-    private val functions = mutableListOf<FunctionDescriptor>()
-    private val properties = mutableListOf<PropertyDescriptor>()
-    private val scopes = mutableListOf<MemberScope>()
+    private val packageScopes = mutableListOf<MemberScope>()
+    private val classScopes = mutableListOf<MemberScope>()
 
     fun addAllFiles(files: Collection<Node.File>) {
         structuredProject += files
     }
 
-    fun addScope(scope: MemberScope) {
-        scopes += scope
+    fun addClassScope(scope: MemberScope) {
+        classScopes += scope
+    }
+
+    fun addPackageScope(scope: MemberScope) {
+        packageScopes += scope
     }
 
     fun onCompleteAnalysis() {
-        findTopLevelDescriptors()
+        processScopes()
+
         // TODO
     }
 
-    private fun findTopLevelDescriptors() {
-        scopes.forEach {
-            findAndAddPropertiesAndFunctions(DescriptorUtils.getAllDescriptors(it))
-        }
-        scopes.clear()
+    private fun processScopes() {
+        val classScopesDescriptors = classScopes.map(::findDescriptors)
+        val packageScopesDescriptors = packageScopes.map(::findDescriptors)
+        classScopes.clear()
+        packageScopes.clear()
     }
 
-    private fun findAndAddPropertiesAndFunctions(descriptors: Collection<DeclarationDescriptor>) {
-        descriptors
+    private fun findDescriptors(
+            scope: MemberScope
+    ): ScopeDescriptors {
+        val publicDescriptors = DescriptorUtils.getAllDescriptors(scope)
                 .filterIsInstance(DeclarationDescriptorWithVisibility::class.java)
                 .filter(DeclarationDescriptorWithVisibility::isEffectivelyPublicApi)
-                .forEach { declarationDescriptor ->
-                    when (declarationDescriptor) {
-                        is PropertyDescriptor -> properties += declarationDescriptor
-                        is FunctionDescriptor -> functions += declarationDescriptor
-                    }
-                }
+        return ScopeDescriptors(
+                properties = publicDescriptors.filterIsInstance<PropertyDescriptor>(),
+                functions = publicDescriptors.filterIsInstance<FunctionDescriptor>(),
+                classes = publicDescriptors.filterIsInstance<LazyClassDescriptor>(),
+                typealiases = publicDescriptors.filterIsInstance<LazyTypeAliasDescriptor>()
+        )
     }
+
+    private data class ScopeDescriptors(
+            val properties: List<PropertyDescriptor>,
+            val functions: List<FunctionDescriptor>,
+            val classes: List<LazyClassDescriptor>,
+            val typealiases: List<LazyTypeAliasDescriptor>
+    )
 
 }
